c250c114d2c36ca8b4ee57699924c6ffb4338be5
Michael30706 <96345221+Michael30706@users.noreply.github.com>
2023-05-12 22:26:32 +0300

    ver2

--- Player.gd
+++ Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
+const SPEED = 1
 const JUMP_VELOCITY = 4.5
 
 
@@ -15,17 +15,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * 0.01)
-			camera.rotate_x(-event.relative.y * 0.01)
+			neck.rotate_y(-event.relative.x * 0.001)
+			camera.rotate_x(-event.relative.y * 0.001)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
-	
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	#	velocity.y -= gravity * delta
-	# Handle Jump.
-
-
+	print("player: ", global_position)
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
