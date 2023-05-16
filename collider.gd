8a4cab46e0aae1a9a60fef6541f218ddadce3f38
GPCcorp <uuknown943@gmail.com>
2023-05-16 15:48:42 +0300

    Fixings

--- collider.gd
+++ collider.gd
@@ -1,10 +1,25 @@
 extends Area3D
 
+var other_mesh_instance: MeshInstance3D
 
 func _ready():
+	var mesh_name = String(get_name())
+	if mesh_name[mesh_name.length() - 1] == 'A':
+		# Change the last letter to 'B'
+		mesh_name = mesh_name.substr(0, mesh_name.length() - 1) + 'B'
+		
+		other_mesh_instance = load("res://Objects/" + mesh_name + ".tscn").instance()
+		
+	elif mesh_name[mesh_name.length() - 1] == 'B':
+		# Change the last letter to 'A'
+		mesh_name = mesh_name.substr(0, mesh_name.length() - 1) + 'A'
+		
+		other_mesh_instance = load("res://Objects/" + mesh_name + ".tscn").instance()
+	
 	var static_body = get_node("static_cube")
 	static_body.set_collision_layer(0)
 	static_body.set_collision_mask(0)
+	
 
 func _on_body_entered(body):
 	print("Entered!!!")
@@ -20,15 +35,14 @@ func _on_body_exited(body):
 	new_mesh.mesh.size = size_coll
 	var local_pos = coll.to_local(pos)
 	new_mesh.set_global_transform(Transform3D(Basis(), local_pos))
-	add_child(new_mesh)   
+	add_child(new_mesh) 
+	
 	print("Exited!!!!")
+	
 	var static_body = get_node("static_cube")
 	static_body.set_collision_layer(1)
 	static_body.set_collision_mask(1)
 	
-
-
-
-
-
-
+	var static_body_of_neighbour_object = other_mesh_instance.get_node("static_cube")
+	static_body.set_collision_layer(1)
+	static_body.set_collision_mask(1)
